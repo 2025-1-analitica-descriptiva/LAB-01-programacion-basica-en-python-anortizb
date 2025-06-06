dbe62a2736b646e8c462ad4714b083415084677e
Andres Ortiz B <145217966+anortizb@users.noreply.github.com>
2025-06-06 18:21:05 -0500

    Finalizado con exito

--- homework/pregunta_05.py
+++ homework/pregunta_05.py
@@ -4,7 +4,7 @@ datos requeridos se encuentran en el archivo data.csv. En este laboratorio
 solo puede utilizar las funciones y librerias basicas de python. No puede
 utilizar pandas, numpy o scipy.
 """
-import os
+
 
 def pregunta_05():
     """
